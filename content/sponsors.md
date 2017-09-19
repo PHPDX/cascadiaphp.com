daa045a015cb4fa99858925101db877bd94d2c82
Korvin Szanto <korvinszanto@gmail.com>
2017-09-19 15:55:17 -0700

    Add a sponsor band to the homepage

---
+++ content/sponsors.md
@@ -0,0 +1,3 @@
+* [![ImpactFlow](/images/sponsors/impactflow.svg)](https://impactflow.com/)
+* [![concrete5](/images/sponsors/concrete5.png)](https://www.concrete5.org/)
+* [![Treehouse](/images/sponsors/treehouse.png)](https://teamtreehouse.com/)
--- templates/home.php
+++ templates/home.php
@@ -15,27 +15,32 @@ $this->layout('layout', [
         <p><?= $this->markdown('mission') ?></p>
     </div>
 </div>
-
-
-<div class="row">
-    <div class="col-6">
-        <h3 class="center">Next Event</h3>
-        <?php
-        $this->insert('events/list', [
-            'eventList' => $eventList,
-            'limit' => 1,
-            'small' => true
-        ]);
-        ?>
-    </div>
-    <div class="col-6">
-        <h3 class="center">Last Event</h3>
-        <?php
-        $this->insert('events/list', [
-            'eventList' => $pastEventList,
-            'limit' => 1,
-            'small' => true
-        ]);
-        ?>
-    </div>
 </div>
+<h2 class="center">Our Sponsors</h2>
+<div class="sponsorband">
+    <?= $this->markdown('sponsors') ?>
+</div>
+<div class="container">
+    <div class="row">
+        <div class="col-6">
+            <h3 class="center">Next Event</h3>
+            <?php
+            $this->insert('events/list', [
+                'eventList' => $eventList,
+                'limit' => 1,
+                'small' => true
+            ]);
+            ?>
+        </div>
+        <div class="col-6">
+            <h3 class="center">Last Event</h3>
+            <?php
+            $this->insert('events/list', [
+                'eventList' => $pastEventList,
+                'limit' => 1,
+                'small' => true
+            ]);
+            ?>
+        </div>
+    </div>
+    <div class="spacer" style="height:50px"></div>
