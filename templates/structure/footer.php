b43cbae651de72947eeebe48cd6e53d4e3619cae
Korvin Szanto <korvinszanto@gmail.com>
2017-09-19 16:33:03 -0700

    Update github link

--- templates/structure/footer.php
+++ templates/structure/footer.php
@@ -5,5 +5,5 @@ $commit = $this->e(substr(getenv('VERSION'), 0, 8));
 
 <div class="footer-floater center">
     Copyright &copy; <?= $this->e(date('Y')); ?> PHPDX &nbsp;
-    Commit: <a href="https://github.com/buttress/phpdx.org/commit/<?= $commit ?>/"><?= $commit ?></a>
+    Commit: <a href="https://github.com/phpdx/phpdx.org/commit/<?= $commit ?>/"><?= $commit ?></a>
 </div>
