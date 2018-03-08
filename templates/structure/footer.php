588dc5342b72bd9b9c65a936148547a10d90e609
Korvin Szanto <korvinszanto@gmail.com>
2018-03-07 19:18:35 -0800

    Link to cascadiaphp rather than phpdx

--- templates/structure/footer.php
+++ templates/structure/footer.php
@@ -5,5 +5,5 @@ $commit = $this->e(substr(getenv('VERSION'), 0, 8));
 
 <div class="footer text-center">
     Copyright &copy; <?= $this->e(date('Y')); ?> Cascadia PHP &nbsp;
-    Commit: <a href="https://github.com/phpdx/phpdx.org/commit/<?= $commit ?>/"><?= $commit ?></a>
+    Commit: <a href="https://github.com/phpdx/cascadiaphp.org/commit/<?= $commit ?>/"><?= $commit ?></a>
 </div>
