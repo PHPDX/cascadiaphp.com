36ff684c6073dd524b3245ecce9ba042a5cf4e94
Korvin Szanto <korvinszanto@gmail.com>
2018-03-28 15:46:17 -0700

    Make sure the hamburger is hidden in md/lg

--- templates/structure/header.php
+++ templates/structure/header.php
@@ -6,7 +6,7 @@
         <amp-img src="/images/logo.svg" height=263 width=705 layout="fill"></amp-img>
     </div>
     <div id="nav_container" class="flex justify-end xs-hide sm-hide"></div>
-    <div class="flex flex-column justify-center">
+    <div class="flex flex-column justify-center md-hide lg-hide">
         <a class="hamburger h3 mr3" on='tap:sidebar.open'>
             <i class="mbri-menu"></i>
         </a>
