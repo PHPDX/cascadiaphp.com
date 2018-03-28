3f1ce7d1a6799377197031861dfb5cd2918273a1
Korvin Szanto <korvinszanto@gmail.com>
2018-03-28 15:52:14 -0700

    Fix header alignment

--- templates/structure/header.php
+++ templates/structure/header.php
@@ -5,7 +5,7 @@
     <div class="flex-auto relative my1 ml3">
         <amp-img src="/images/logo.svg" height=263 width=705 layout="fill"></amp-img>
     </div>
-    <div id="nav_container" class="flex justify-end xs-hide sm-hide"></div>
+    <div id="nav_container" class="flex justify-end xs-hide sm-hide mr1"></div>
     <div class="flex flex-column justify-center md-hide lg-hide">
         <a class="hamburger h3 mr3" on='tap:sidebar.open'>
             <i class="mbri-menu"></i>
