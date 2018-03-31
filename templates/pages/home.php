a78ad8fe2e94c226555d7f31554420620183599a
Your Name <you@example.com>
2018-03-31 15:13:02 -0700

    Use gravel for border separators

--- templates/pages/home.php
+++ templates/pages/home.php
@@ -31,7 +31,7 @@
         </div>
     </div>
     <div class="flex fold-cta bg-white darkblue justify-center">
-        <div class="col md-col-4 sm-col-6 xs-hide my4 px3 border-right b-gold b3 relative">
+        <div class="col md-col-4 sm-col-6 xs-hide my4 px3 border-right b-gravel b3 relative">
             <amp-fit-text max-font-size="50px" height="80px" layout="flex-item" class="flex-auto center">
                 <span class="bold">2</span> days
             </amp-fit-text>
@@ -39,7 +39,7 @@
                 <span class="bold">250+</span> tickets
             </amp-fit-text>
         </div>
-        <div class="col col-4 sm-hide xs-hide my4 px3 border-right b-gold b3 relative">
+        <div class="col col-4 sm-hide xs-hide my4 px3 border-right b-gravel b3 relative">
             <amp-fit-text max-font-size="50px" height="80px" layout="flex-item" class="flex-auto center">
                 <span class="bold">250+</span> tickets
             </amp-fit-text>
@@ -53,7 +53,7 @@
 </section>
 <section class="where bg-dark pb3">
     <h1 class="m0 p2 pt4 center bold white">Portland Oregon</h1>
-    <div class="center pb2 white">Where the dream of the 90s is still alive</div>
+    <div class="center pb2 white">Some subtext about Portland that is creative</div>
 
     <amp-carousel height="300" layout="fixed-height" type="carousel" class="bg-dark">
         <amp-img src="https://placeimg.com/400/300/arch" width="400" height="300" alt="a sample image"></amp-img>
