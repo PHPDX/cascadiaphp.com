a15a6ff8bc45d20a20f8e37de5f49782b6d12d2a
Korvin Szanto <korvinszanto@gmail.com>
2018-04-07 13:58:47 -0700

    Adjust titles and descriptions for all pages

--- src/Controller/Speakers.php
+++ src/Controller/Speakers.php
@@ -12,6 +12,11 @@ class Speakers extends Controller
 
     public function view(): Template
     {
+        $this->seo()
+            ->setTitle('Awesome speakers for Cascadia PHP 2018')
+            ->setDescription('Our speaker lineup for Cascadia PHP 2018 in Portland Oregon. Check here for a list of ' .
+                'the industry experts we are shipping in to speak to our community.');
+
         return $this->render('/pages/speakers');
     }
 }
