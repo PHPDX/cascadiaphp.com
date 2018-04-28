0a8bd3c10445a9f0fa5d255fd262a71ba2a69e8b
Korvin Szanto <korvinszanto@gmail.com>
2018-04-27 18:29:44 -0700

    Make sure the thing has a shadow

--- templates/structure/sponsor.php
+++ templates/structure/sponsor.php
@@ -6,7 +6,7 @@
 <?php endif ?>
 
 <div class="flex p2 sponsor">
-    <amp-layout layout="responsive" width="1" height="1" sizes="(min-width: 40em) calc((1440px - 14em) / 3), calc(100vw - 6em)" class="shadow-md shadow-inner circle b-<?= $borderColor ?? 'black' ?> bg-<?= $backgroundColor ?? 'white' ?>">
+    <amp-layout layout="responsive" width="1" height="1" sizes="(min-width: 40em) calc((1440px - 14em) / 3), calc(100vw - 6em)" class="shadow-md circle b-<?= $borderColor ?? 'black' ?> bg-<?= $backgroundColor ?? 'white' ?>">
         <amp-img class="object-cover object-contain m3"
                  src="<?= $logo ?>"
                  layout="fill"
