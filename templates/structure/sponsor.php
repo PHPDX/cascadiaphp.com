f940ebd20e9b418451574abf17849e2ce7b5d6c8
Korvin Szanto <korvinszanto@gmail.com>
2018-04-27 18:24:32 -0700

    Add OSMI to our sponsor page

--- templates/structure/sponsor.php
+++ templates/structure/sponsor.php
@@ -1,11 +1,16 @@
 <?php declare(strict_types=1) ?>
 <?php /** @var \CascadiaPHP\Site\Template\Template $this */ ?>
 
+<?php if (isset($link)): ?>
+    <a href="<?= $link ?>">
+<?php endif ?>
+
 <div class="flex p2 sponsor">
-    <amp-layout layout="responsive" width="1" height="1" sizes="(min-width: 40em) 200px, calc(100vw - 6em)" class="circle b-<?= $borderColor ?? 'black' ?> bg-<?= $backgroundColor ?? 'white' ?>">
+    <amp-layout layout="responsive" width="1" height="1" sizes="(min-width: 40em) calc((1440px - 14em) / 3), calc(100vw - 6em)" class="shadow-md shadow-inner circle b-<?= $borderColor ?? 'black' ?> bg-<?= $backgroundColor ?? 'white' ?>">
         <amp-img class="object-cover object-contain m3"
                  src="<?= $logo ?>"
-                 layout="fill">
+                 layout="fill"
+                 alt="<?= $alt ?>">
             <?php if ($fallback ?? false): ?>
                 <div fallback>
                     <?= $fallback ?>
@@ -17,3 +22,7 @@
         <span class="mbri-star local-icon gold h3"></span>
     <?php endif; ?>
 </div>
+
+<?php if (isset($link)): ?>
+    </a>
+<?php endif ?>
