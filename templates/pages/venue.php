f023d91a337867a6f76fb2b268167671c924f84d
Korvin Szanto <korvinszanto@gmail.com>
2018-06-19 09:13:55 -0700

    Make map butt up against the HR

--- templates/pages/venue.php
+++ templates/pages/venue.php
@@ -6,9 +6,10 @@ $this->start('components') ?>
 <script async custom-element="amp-iframe" src="https://cdn.ampproject.org/v0/amp-iframe-0.1.js"></script>
 <?php $this->stop() ?>
 
-<h1 class="my0 mb3 cubes pb2 pt3 border-bottom b2 b-gold bg-white px3 darkblue">Venue</h1>
+<h1 class="my0 cubes pb2 pt3 border-bottom b2 b-gold bg-white px3 darkblue">Venue</h1>
 
 <amp-iframe
+        class="mb3"
         width="1000"
         height="250"
         sandbox="allow-scripts allow-same-origin"
