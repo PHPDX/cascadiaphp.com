fe1a72e9d6dfb3d71f04a8a185c454706525b43b
Korvin Szanto <korvinszanto@gmail.com>
2019-04-08 18:58:42 -0700

    Add twitter card and opengraph support

--- public/packages/cascadiaphp/themes/cascadiaphp/elements/header_top.php
+++ public/packages/cascadiaphp/themes/cascadiaphp/elements/header_top.php
@@ -1,11 +1,48 @@
 <?php defined('C5_EXECUTE') or die('Access Denied.');
-/** @var \Concrete\Core\View\View $view */
+/** @var Concrete\Core\View\View $view */
 $view = $view ?? null;
+/** @var Concrete\Core\Page\Page $c */
+$c = $c ?? null;
 ?>
 <!doctype html>
 
 <html lang="<?= Concrete\Core\Localization\Localization::activeLanguage() ?>">
 <head>
     <?php $view->element('header_required') ?>
+
+    <?php
+    $opengraph = new Artesaos\SEOTools\OpenGraph();
+    $twitter = new Artesaos\SEOTools\TwitterCards();
+
+    $opengraph->setType('website');
+    $twitter->setType('summary_large_image');
+
+    $site = $c->getSite();
+    $opengraph->setSiteName($site->getSiteName());
+    $twitter->setSite('@cascadiaphp');
+
+    $title = $c->getAttribute('meta_title') ?? $c->getCollectionName();
+    $opengraph->setTitle($title);
+    $twitter->setTitle($title);
+
+    $description = $c->getAttribute('meta_description') ?? $c->getAttribute('description');
+    if ($description) {
+        $opengraph->setDescription($description);
+        $twitter->setDescription($description);
+    }
+
+    // If there's a thumbnail image set for this page
+    $thumbnail = $c->getAttribute('page_thumbnail');
+    if ($thumbnail) {
+        if ($thumbnail instanceof Concrete\Core\Entity\File\File) {
+            $url = $thumbnail->getVersion()->getURL();
+            $twitter->setImage($url);
+            $opengraph->addImage($url);
+        }
+    }
+
+    echo $opengraph->generate();
+    echo $twitter->generate();
+    ?>
 </head>
 <body>
